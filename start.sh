2455f11b47b5b51316c432fc45640dcdd2dcc2f2
Timothy <tim.zhuravlev@5thkind.com>
2016-06-21 21:45:02 +0300

    Change config file

--- start.sh
+++ start.sh
@@ -9,7 +9,7 @@ cd  /home/troll/works/code/mojo/queue
 perl /usr/local/bin/morbo script/queue reload --listen 'http://*:3000' 
 #exit 1
 #read -n 1
-#perl /usr/local/bin/hypnotoad script/epp_client --listen http://*:4000 &
+#perl /usr/local/bin/hypnotoad script/queue --listen http://*:3000 &
 
 
 # sudo /usr/sbin/service nginx reload
